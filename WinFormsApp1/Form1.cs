5fcb1c3dd9456f1513339892bf14fcfe0127766c
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-05-17 14:03:25 +0900

    Initial commit

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ç≤ãvä‘ óIê∂");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A601-12");
+        }
+    }
+}
